8ed13d30ef30195009bb3133d9fa1ff31b6c2812
wrathematics <wrathematics@gmail.com>
2020-04-05 18:42:15 -0400

    update crlapply api to match the tasktools version

--- R/crlapply.r
+++ R/crlapply.r
@@ -1,40 +1,46 @@
 #' crlapply
 #' 
 #' An \code{lapply()}-like interface with automatic checkpoint/restart
-#' functionality.
+#' functionality. Checkpoint/restart is a useful strategy for running expensive
+#' functions in constrained environments (non-reliable hardware, restricted time
+#' limits, etc).
+#' 
+#' See the package README for a substantive example and explanation.
 #' 
 #' @details
-#' The checkpoint file is cleaned up on successful completion of
-#' \code{crlapply()}
+#' The checkpoint file is removed on successful completion.
 #' 
 #' @param X,FUN,...
 #' Same as in \code{lapply()}.
-#' @param FILE
+#' @param checkpoint_file
 #' The checkpoint file.
-#' @param FREQ
-#' The checkpoint frequency; a positive integer.
+#' @param checkpoint_freq
+#' The checkpoint frequency, i.e. how often the function will save checkpoints.
+#' A frequency of 1 means that saves occur after every evaluation, 2 after
+#' every other, 3 every third, and so on. Should a positive integer.
 #' 
 #' @return
 #' A list.
 #' 
+#' @seealso \code{\link{crsapply}}
 #' @export
-crlapply = function(X, FUN, FILE, FREQ=1, ...)
+crlapply = function(X, FUN, ..., checkpoint_file, checkpoint_freq=1)
 {
   if (missing(X))
     stop("argument 'X' is missing, with no default")
   if (missing(FUN))
     stop("argument 'FUN' is missing, with no default")
-  if (missing(FILE))
-    stop("argument 'FILE' is missing, with no default")
+  if (missing(checkpoint_file))
+    stop("argument 'checkpoint_file' is missing, with no default")
   
-  FREQ = as.integer(FREQ)
-  if (is.na(FREQ) || length(FREQ) != 1L || FREQ < 1L)
-    stop("argument 'FREQ' must be a positive integer")
+  checkpoint_freq = as.integer(checkpoint_freq)
+  if (is.na(checkpoint_freq) || length(checkpoint_freq) != 1L || checkpoint_freq < 1L)
+    stop("argument 'checkpoint_freq' must be a positive integer")
   
   n = length(X)
   
-  if (file.exists(FILE))
-    load(file=FILE)
+  if (file.exists(checkpoint_file))
+    load(file=checkpoint_file)
   else
   {
     start = 1L
@@ -43,17 +49,16 @@ crlapply = function(X, FUN, FILE, FREQ=1, ...)
   
   for (i in start:n)
   {
-    ret[[i]] = FUN(i, ...)
+    ret[[i]] = FUN(X[i], ...)
     
-    if (n %% FREQ == 0)
+    if (n %% checkpoint_freq == 0)
     {
       start = i+1L
-      save(start, ret, file=FILE)
+      save(start, ret, file=checkpoint_file)
     }
   }
   
   
-  file.remove(FILE)
-  
+  file.remove(checkpoint_file)
   ret
 }
